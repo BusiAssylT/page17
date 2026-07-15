644fcf7211e5806f94eeecb8b0e7bdcc9bcca32d
BusiAssylT <businessassylt@gmail.com>
2026-07-15 21:39:58 +0500

    Connect ticketing to Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PAGE17_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://zkpliibzhedphljjmrma.supabase.co",
+  supabaseAnonKey: "sb_publishable_22S264HM0384Ee2jvmPvUQ_2Qu3ye46",
   siteUrl: window.location.origin + window.location.pathname.replace(/\/[^/]*$/, "")
 };
